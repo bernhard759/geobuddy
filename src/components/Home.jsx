1b3b2117f82e1080c362df9ac7d4afdeb10ce3e4
Bernhard Gailer <bernhard.gailer@gmx.de>
2024-10-30 12:15:33 +0100

    add lottiefile

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,21 @@
 // src/components/Home.js
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Lottie from "lottie-react";
+import animationData from '../assets/animation.json';
+import "./Home.css";
 
 const Home = () => {
   return (
     <div className="text-center">
+  <div className="animation-container">
+      <Lottie animationData={animationData} loop={true} />
+    </div>
+
       <h1 className="text-4xl font-bold mb-4">Welcome to GeoBuddy!</h1>
       <p className="text-lg mb-6">Learn the capitals all around the globe with your adaptive online buddy.</p>
+      
+      
       <div className="flex justify-center gap-6">
 
         {/* Text quiz */}
